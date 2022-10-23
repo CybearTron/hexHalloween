5d613df8ffe02fb49f7e0fa1bc303f538a314940
Cybear Tron <vinutron23@gmail.com>
2022-10-23 11:42:11 +0530

    vb

--- hexHalloweenGame/Scripts/Player/Player.gd
+++ hexHalloweenGame/Scripts/Player/Player.gd
@@ -2,9 +2,16 @@ extends KinematicBody2D
 
 var timeline = ""
 
+var butlerDialogged = false #Has Kulrik talked with the butler
+
+var claustroDialogged = false #Has Kulrik talked with Claustro
+
+var beginnerDialogged = false #Has Kulrik talked with Himself in the beginning
+
 export (int) var sanity = 100
 
 func _ready():
+	if beginnerDialogged== false
 	timeline = "Beginning"
 	var new_dialog = Dialogic.start(timeline)
 	add_child(new_dialog)
@@ -45,13 +52,15 @@ func _physics_process(delta):
 
 
 func _on_Area2D_area_entered(area):
-	if area.is_in_group("claustro"):
+	if area.is_in_group("claustro") and claustroDialogged == false:
 		sanity -= 20
 		timeline = "Claustro"
 		var new_dialog = Dialogic.start(timeline)
 		add_child(new_dialog)
+		claustroDialogged = true
 	
-	if area.is_in_group("butler"):
+	if area.is_in_group("butler") and butlerDialogged == false:
 		timeline = "Butler"
 		var new_dialog = Dialogic.start(timeline)
 		add_child(new_dialog)
+		butlerDialogged = true
